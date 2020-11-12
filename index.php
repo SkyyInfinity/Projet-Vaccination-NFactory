9eaf412fd6c73848b07eadcb7814b094513c80ca
Dylan Hautecoeur <dylan.hautecoeur@gmail.com>
2020-11-12 16:50:36 +0100

    ajout des images + page réalisé -> header.php, footer.php, index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
 <?php
+include('inc/pdo.php');
+include('inc/functions.php');
+$title = 'Accueil';
+
+include('inc/header.php');
+
+?>
+    <div class="hero">
+        <img src="assets/img/accueil_hero_opacity.jpg" alt="Docteur qui montre des vaccins">
+        <div class="info">
+            <h1 class="info-title">Besoin d'une gestion de vos vaccins ou d'un rappel pour ne pas oublié ?</h1>
+            <a href="inscription.php" class="btn info-btn">S'inscrire maintenant</a>
+        </div>
+    </div>
+    <section class="section-explications">
+        <div class="wrap">
+            <h2>En quoi consiste le site ?</h2>
+            <hr>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quaerat eaque hic facere vitae amet, et magnam, dicta quasi ea labore! Iure ipsam sunt nulla quia quo fugit velit voluptatibus blanditiis, facilis et est tenetur nam doloremque omnis sapiente excepturi? Voluptate, rem accusamus necessitatibus vitae saepe voluptatem eos veniam tempora aliquam dolorem suscipit at sed, dolores autem quasi et? Dolorum beatae facilis debitis molestias asperiores recusandae sed culpa incidunt voluptatem? Quidem asperiores rem repellendus commodi eligendi minima similique fuga! Voluptas tempore laudantium assumenda, modi dolores expedita laborum pariatur hic commodi? Corporis consectetur numquam autem eveniet ducimus modi quidem tenetur cupiditate?</p>
+        </div>
+    </section>
+    <div class="wrap">
+<?php
+
+include('inc/footer.php');

18d6bf49dd88e08548ce14531420b8c071cc50af
Dylan Hautecoeur <dylan.hautecoeur@gmail.com>
2020-11-12 18:50:46 +0100

    changement de dossier

---
+++ inc/footer.php
@@ -0,0 +1,43 @@
+
+            </div>
+        </div>
+
+        <!-- FOOTER -->
+        <footer id="footer">
+            <div class="wrap">
+                <div class="footer-info">
+                    <div class="company">
+                        <ul>
+                            <li>
+                                <p><span>Adresse:</span> 19 avenue de la médecine,<br>FICTIFVILLE<br>55684</p>
+                                <p><span>Email:</span><a href="mailto:contact@myvaccine.org">contact@myvaccine.org</a></p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="navigation-footer">
+                        <nav>
+                            <ul>
+                                <li><a href="index.php">Accueil</a></li>
+                                <li><a href="mentions-legales.php">Mentions Légales</a></li>
+                                <li><a href="contact.php">Contact</a></li>
+                            </ul>
+                            <div class="logo">
+                                <a href="index.php"><img src="assets/img/myvaccine_x200.png" alt="Logo MyVaccine"></a>
+                            </div>
+                        </nav>
+                    </div>
+                    <div class="social">
+                        <ul class="social-links">
+                            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <p class="copyright">&copy; Copyright MyVaccine.org 2020 | Tous droits reservés</p>
+            </div>
+            <div class="border-gradient"></div>
+        </footer>
+    </body>
+</html>

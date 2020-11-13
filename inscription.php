8a25f82e4290bce1361efd65ee46cbda350a7e56
Oummy13 <Oummy20180@gmail.com>
2020-11-13 10:44:05 +0100

    ajout de page >connexion.php, inscription.php

---
+++ inscription.php
@@ -0,0 +1,161 @@
+<?php
+include('inc/pdo.php');
+include('inc/functions.php');
+
+$errors = array();
+$success = false;
+// traitement php
+if (!empty($_POST['submitinscription'])) {
+  // Faille
+  $nom = cleanXss($_POST['nom']);
+  $prenom = cleanXss($_POST['prenom']);
+  $email = cleanXss($_POST['email']);
+  $password1 = cleanXss($_POST['password1']);
+  $password2 = cleanXss($_POST['password2']);
+  // validationText
+  $errors = validationText($errors,$nom,'nom',4,50);
+  $errors = validationText($errors,$prenom,'prenom',3,50);
+
+  // validation email(email valide, unique)
+  if(!empty($email)) {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $errors['email'] =  'Veuillez renseigner un email valide';
+    }
+    else {
+      $sql = "SELECT id FROM users WHERE email = :email";
+      $query = $pdo->prepare($sql);
+      $query->bindValue(':email',$email,PDO::PARAM_STR);
+      $query->execute();
+      $verifEmail = $query->fetch();
+      if(!empty($verifEmail)) {
+        $errors['email'] = 'Cet email existe déjà';
+      }
+    }
+  }
+  else {
+    $errors['email'] = 'Veuillez renseigner un email';
+  }
+
+  // validtion password
+  if (!empty($password1) && !empty($password2)) {
+    if ($password1 != $password2) {
+      $errors['password'] = 'Veillez renseigner des mots de passes identiques';
+    }
+  }
+
+      $majuscule        = preg_match('@[A-Z]@', $password1);
+      $minuscule        = preg_match('@[a-z]@', $password1);
+      $chiffre          = preg_match('@[0-9]@', $password1);
+      $caractereSpecial = preg_match('@[^\w]@', $password1);
+      if(!empty($password1)){
+        if(mb_strlen($password1) < 5) {
+          $errors['password'] = 'Le mot de passe doit être plus grand que 5 caractères.';
+        } elseif(mb_strlen($password1) > 45) {
+          $errors['password'] = 'Le mot de passe doit être plus petit que 45 caractères.';
+        }
+         elseif(!$majuscule || !$minuscule || !$chiffre || !$caractereSpecial) {
+          $errors['password'] = 'Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractére spécial.';
+        }
+      }
+
+       else {
+        $errors['password'] = 'Veuillez renseigner ce champ.';
+      }
+
+      $majuscule        = preg_match('@[A-Z]@', $password2);
+      $minuscule        = preg_match('@[a-z]@', $password2);
+      $chiffre          = preg_match('@[0-9]@', $password2);
+      $caractereSpecial = preg_match('@[^\w]@', $password2);
+      if(!empty($password2)){
+        if(mb_strlen($password2) < 5) {
+          $errors['password'] = 'Le mot de passe doit être plus grand que 5 caractères.';
+        } elseif(mb_strlen($password2) > 45) {
+          $errors['password'] = 'Le mot de passe doit être plus petit que 45 caractères.';
+        }
+        elseif(!$majuscule || !$minuscule || !$chiffre || !$caractereSpecial) {
+          $errors['password'] = 'Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractére spécial.';
+        }
+
+      }else {
+        $errors['password'] = 'Veuillez renseigner ce champ.';
+      }
+
+
+
+
+  if(count($errors) == 0)
+  {
+    $hashPassword= password_hash($password1, PASSWORD_DEFAULT);
+
+    $role = 'abonne';
+
+    $token = generateRandomString(120);
+
+    // INSERT
+
+    $sql = "INSERT INTO users (nom, prenom, email, password, created_at, token, role) VALUES  (:nom, :prenom, :email, :password, NOW(), '$token', '$role')";
+    $query = $pdo -> prepare($sql);
+    $query ->bindValue(':nom', $nom, PDO::PARAM_STR);
+    $query ->bindValue(':prenom', $prenom, PDO::PARAM_STR);
+    $query ->bindValue(':email', $email, PDO::PARAM_STR);
+    $query ->bindValue(':password', $hashPassword, PDO::PARAM_STR);
+    $query->execute();
+
+
+    $success = true;
+
+    // header('Location: index.php');
+
+
+  }
+}
+// debug($errors);
+// debug($_POST);
+include('inc/header.php');?>
+<h1>Inscription</h1>
+
+
+<?php if ($success == true) {
+  echo '<h1>votre inscription a bien été prise en compte ! </h1>';
+}
+else { ?>
+  <form id="forminscrit" action="" method="post" novalidate>
+  <div class="form1">
+    <label for="name">Nom* </label>
+    <input type="text" name="nom"  id="name" value="<?php if (!empty($_POST['nom'])){echo $_POST['nom'];} ?>">
+    <span class="error"><?php if (!empty($errors['nom'])){echo $errors['nom'];} ?></span>
+  </div>
+  <div class="form1">
+
+    <label for="prenom">Prénom* </label>
+    <input type="text" name="prenom"  id="prenom" value="<?php if (!empty($_POST['prenom'])){echo $_POST['prenom'];} ?>">
+    <span class="error"><?php if (!empty($errors['prenom'])){echo $errors['prenom'];} ?></span>
+  </div>
+
+  <div class="form1">
+    <label for="mail">E-mail* </label>
+    <input type="email" name="email"  id="mail" value="<?php if (!empty($_POST['email'])){echo $_POST['email'];} ?>">
+    <span class="error"><?php if (!empty($errors['email'])){echo $errors['email'];} ?></span>
+  </div>
+
+  <div class="form1">
+    <label for="mdp">Mot de passe *</label>
+    <input type="password" name="password1"  id="mdp" value="<?php if (!empty($_POST['password1'])){echo $_POST['password1'];} ?>">
+    <span class="error"><?php if (!empty($errors['password'])){echo $errors['password'];} ?></span>
+  </div>
+
+  <div class="form1">
+    <label for="mdp2">Confirmation Mot de passe *</label>
+    <input type="password" name="password2"  id="mdp2" value="<?php if (!empty($_POST['password2'])){echo $_POST['password2'];} ?>">
+    <span class="error"><?php if (!empty($errors['password'])){echo $errors['password'];} ?></span>
+  </div>
+    <input type="submit" name="submitinscription" value="S'inscrire">
+  </div>
+
+  </form>
+
+<?php } ?>
+
+
+<?php
+include('inc/footer.php');

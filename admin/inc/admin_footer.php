2a776c23cfe9b082184b745ed017560ab996681c
yohannmts <yohannmats@gmail.com>
2020-11-13 11:47:49 +0100

    ajout adminindex

---
+++ admin/inc/admin_footer.php
@@ -0,0 +1,36 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; MyVaccine.org 2020 | Tous droits reservés</span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
+
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Logout Modal-->
+
+
+<!-- Bootstrap core JavaScript-->
+<script src="assets/panel_admin/vendor/jquery/jquery.min.js"></script>
+<script src="assets/panel_admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="assets/panel_admin/js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>

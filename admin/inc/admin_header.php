783996a5128c1584a1e2e64d68e1ae065c6ef477
yohannmts <yohannmats@gmail.com>
2020-11-17 12:46:09 +0100

    modification ds liens de la page d'accueil back + ajout onglet messagerie

--- admin/inc/admin_header.php
+++ admin/inc/admin_header.php
@@ -31,11 +31,11 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../index.php">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="-brand-text mx-3">ACCUEIL</div>
+                <div class="-brand-text mx-3">ACCUEIL ADMIN</div>
             </a>
 
             <!-- Divider -->
@@ -43,9 +43,9 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item">
-                <a class="nav-link" href="admin_index.php">
+                <a class="nav-link" href="../index.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Tableau de bord</span></a>
+                    <span>RETOUR AU SITE </span></a>
             </li>
 
             <!-- Divider -->
@@ -55,38 +55,28 @@
 
             <!- Nav Item - Pages Collapse Menu -->
 
-            <li class="nav-item active">
-                <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
-                    aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Pages de connexion </span>
-                </a>
-                <div id="collapsePages" class="collapse show" aria-labelledby="headingPages"
-                    data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Pages de connexion :</h6>
-                        <a class="collapse-item" href="login.html">Connexion</a>
-                        <a class="collapse-item" href="register.html">Inscription</a>
 
-                        <div class="collapse-divider"></div>
-
-
-                    </div>
-                </div>
-            </li>
-
-            <!-- Nav Item - Charts -->
+            <!-- Nav Item - STATISTIQUES -->
             <li class="nav-item">
                 <a class="nav-link" href="statistiques.php">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Statistiques</span></a>
             </li>
-            <!-- Nav Item - Tables -->
             <hr class="sidebar-divider d-none d-md-block">
+            <!-- Nav Item - GESTION VACCINS-->
+            
+            
             <li class="nav-item">
                 <a class="nav-link" href="vaccins.php">
                     <i class="fas fa-fw fa-table"></i>
                     <span>Gérer les vaccins</span></a>
+            </li>
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <li class="nav-item">
+                <a class="nav-link" href="messagerie.php">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Messagerie</span></a>
             </li>
             <hr class="sidebar-divider d-none d-md-block">
 
